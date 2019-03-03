a1fed4d6d2ed71a9c1018b62edd2e4262d1581cb
jonathan.sawyer <jonathan.sawyer@linecorp.com>
2019-03-03 15:11:39 +0900

    compare triplets

---
+++ solutions/python/compare-the-triplets/main.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    alice = 0
+    bob = 0
+    for index in range(len(a)):
+        if(a[index] > b[index]):
+            alice += 1
+        elif b[index] > a[index]: 
+            bob += 1
+    return [alice, bob]
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
